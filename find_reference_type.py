88efc4705fa19c1b4661951acccb90af18933f48
Sara.moazezz@gmail.com <Sara.moazezz@gmail.com>
2022-09-30 17:39:17 +0200

    added so that we can go through all pdfs in dataset directory and classify references, all unclassified references will be written to txt files for manual classifying later

--- find_reference_type.py
+++ find_reference_type.py
@@ -1,34 +1,55 @@
 from PyPDF2 import PdfReader
 from nltk.tokenize import word_tokenize
 import re
+import os
 
-pdfFileObj = open("./dataset/1534466-FULLTEXT01-refpages.pdf",'rb')
 
-reader = PdfReader(pdfFileObj)
+def get_reference_types(file):
+    pdfFileObj = open("./dataset/"+file,'rb')
 
-output = ""
-for i in range(reader.numPages):
-    pageObj = reader.getPage(i)
-    output += pageObj.extractText()
+    reader = PdfReader(pdfFileObj)
 
-#print(output)
-pattern = r'[+[0-9]+]'
-splitText = re.split(pattern, output)
-#print(len(splitText))
+    output = ""
+    for i in range(reader.numPages):
+        pageObj = reader.getPage(i)
+        output += pageObj.extractText()
 
-volumeNumber = r'[0-9]+\.+[0-9]'
-volumeNumberWithParantheses = r'[0-9]+\(+(?<!\d)\d{1,3}(?!\d)+\)'
-journal = 0
-isbn = 0
-conference = 0
-for reference in splitText:
-    if ("journal" in reference or "Journal" in reference or "no." in reference or re.search(volumeNumber, reference)
-     or re.search(volumeNumberWithParantheses, reference) or "ISBN" in reference or "conference" in reference
-     or "Conference" in reference or "symposium" in reference or "Symposium" in reference):
-        if "ISBN" in reference:
-            isbn += 1
-        elif  ("conference" in reference or "Conference" in reference or "symposium" in reference or "Symposium" in reference):
-            conference += 1
+    #print(output)
+    pattern = r'\[[0-9]+\]'
+    splitText = re.split(pattern, output)
+    #print(len(splitText))
+
+    volumeNumber = r'[0-9]+\.+[0-9]'
+    volumeNumberWithParantheses = r'[0-9]+\(+(?<!\d)\d{1,3}(?!\d)+\)'
+    url = r'(http(s)?)|(www)'
+    webPage = 0
+    journal = 0
+    isbn = 0
+    conference = 0
+    for reference in splitText:
+        #print(reference)
+        if ("journal" in reference or "Journal" in reference or "no." in reference or re.search(volumeNumber, reference)
+        or re.search(volumeNumberWithParantheses, reference) or "ISBN" in reference or "conference" in reference
+        or "Conference" in reference or "symposium" in reference or "Symposium" in reference 
+        or ("Accessed" in reference and re.search(url, reference))):
+            if "ISBN" in reference:
+                isbn += 1
+            elif  ("conference" in reference or "Conference" in reference or "symposium" in reference or "Symposium" in reference):
+                conference += 1
+            elif ("Accessed" in reference and re.search(url, reference)):
+                webPage += 1
+            else:
+                journal += 1
         else:
-            journal += 1
-print("occurrences of journal: " , journal , "occurrences of isbn: " , isbn, "occurrences of conference: ", conference)
+            unclassifiedReferences = open(file[0:17]+".txt", "a", encoding="utf-8")
+            unclassifiedReferences.write(reference)
+            unclassifiedReferences.write("\n")
+            unclassifiedReferences.close()
+
+    print("Total references: ", (len(splitText)-1), "Number of unclassified references: ", ((len(splitText)-1)-(journal+conference+isbn+webPage)))
+    print("occurrences of journal: " , journal , "occurrences of isbn: " , isbn, "occurrences of conference: ", conference, 
+            "occurrences of webpages: " , webPage)
+
+files = os.listdir("./dataset")
+for file in files:
+    get_reference_types(file)
